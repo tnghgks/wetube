45f41bfb22a5f95e54e9da2fe647222b5ff3e62c
tnghgks <tnghgks@naver.com>
2021-03-31 13:14:18 +0000

    end

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
-
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req , res) => res.send('Hello from home');
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended : true }));
@@ -19,11 +16,8 @@ app.use(bodyParser.json());
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
-
+app.use("/", globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
---
+++ routers/videoRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import routes from "../routes";
+
+const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, (req, res) => res.send("VIDEOS"));
+videoRouter.get(routes.upload, (req, res) => res.send("UPLOAD"));
+videoRouter.get(routes.videoDetail, (req, res) => res.send("VIDEO_DETAIL"));
+videoRouter.get(routes.editVideo, (req, res) => res.send("EDIT_VIDEO"));
+videoRouter.get(routes.deleteVideo, (req, res) => res.send("DELETE_VIDEO"));
+
+export default videoRouter;
