24c29f61e40584c826fe972a5534d422a57e30f8
tnghgks <tnghgks@naver.com>
2021-04-06 18:10:23 +0000

    04/07 commit

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # WeTube
 
 Cloning Youtube with Vanilla and NodeJS# wetube
+
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [X] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload Video
+- [ ] Video Detail
+- [ ] Edit Vedio
+
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,18 @@
 import express from "express";
 import routes from "../routes";
+import {
+  videos,
+  upload,
+  videoDetail,
+  editVideo,
+  deleteVideo
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.send("VIDEOS"));
-videoRouter.get(routes.upload, (req, res) => res.send("UPLOAD"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("VIDEO_DETAIL"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("EDIT_VIDEO"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("DELETE_VIDEO"));
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
